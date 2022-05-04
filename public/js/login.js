1be989e597f9f61295ae7b72644899378cf07fd8
ssun97 <sunmingyu.97@gmail.com>
2022-05-04 01:46:13 -0500

    build bundle

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'post',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -27,7 +27,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+      url: '/api/v1/users/logout'
     });
     if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
@@ -35,16 +35,3 @@ export const logout = async () => {
     showAlert('error', 'Error logging out! Try again.');
   }
 };
-// export const logout = async () => {
-//   try {
-//     const res = await axios({
-//       method: 'GET',
-//       url: 'http://127.0.0.1:3000/api/v1/users/logout',
-//     });
-//     // console.log(res);
-//     if ((res.data.status === 'success')) location.reload(true);
-//   } catch (error) {
-//     console.log(error.response);
-//     showAlert('error', 'Error logging out!');
-//   }
-// };
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,7 +7,7 @@ export const bookTour = async tourID => {
     try {
         const stripe = await loadStripe('pk_test_51KvXAQEe0nwrWacBy1cYng4LATKAPcWOjLcYxIGFG2DBZWu9uVrFF5N4Ju3uOiFbmAjSMOxbmBISAHvIeYebJJgZ00H0fW8a9L');
         // 1) get checkout session from API
-        const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourID}`);
+        const session = await axios(`/api/v1/bookings/checkout-session/${tourID}`);
         // console.log(session);
         // 2) create checkout from + charge credit card
         await stripe.redirectToCheckout({
